69f2f597b9ef8a95d5f8a1a881f37cb59760d2a5
Muhammad Amien <muhammadamien23@gmail.com>
2026-02-27 03:58:24 +0700

    add initial Fiber test and update main route response

---
+++ fiber_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"io"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFiber(t *testing.T) {
+	app := fiber.New()
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("Hello buddy")
+	})
+
+	request := httptest.NewRequest("GET", "/", nil)
+	response, err := app.Test(request)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err := io.ReadAll(response.Body)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello buddy", string(bytes))
+
+}
