751e48f6496c2d945a381ee9b2a9c7756e85c7b0
Muhammad Amien <muhammadamien23@gmail.com>
2026-02-27 04:20:40 +0700

    refactor: move Fiber app initialization outside of test functions

--- fiber_test.go
+++ fiber_test.go
@@ -9,8 +9,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var app = fiber.New()
+
 func TestFiber(t *testing.T) {
-	app := fiber.New()
 	app.Get("/", func(ctx *fiber.Ctx) error {
 		return ctx.SendString("Hello buddy")
 	})
@@ -25,5 +26,33 @@ func TestFiber(t *testing.T) {
 
 	assert.Nil(t, err)
 	assert.Equal(t, "Hello buddy", string(bytes))
+
+}
+
+func TestCtx(t *testing.T) {
+
+	app := fiber.New()
+	app.Get("/hello", func(ctx *fiber.Ctx) error {
+		name := ctx.Query("name", "Guest")
+		return ctx.SendString("Hello " + name)
+	})
+
+	request := httptest.NewRequest("GET", "/hello?name=Amien", nil)
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, _ := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello Amien", string(bytes))
+
+	request = httptest.NewRequest("GET", "/hello", nil)
+	response, err = app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, _ = io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello Guest", string(bytes))
 
 }
